503d15baea3530edccbd7bebf1cabf5ccb2650b9
Irori235 <Irori235@gmail.com>
2024-02-20 18:21:30 +0900

    ✨ Add cron app

---
+++ apps/cron/go.mod
@@ -0,0 +1,14 @@
+module github.com/Irori235/every-second-cronjob/apps/cron
+
+go 1.21.1
+
+require (
+	github.com/go-resty/resty/v2 v2.11.0
+	github.com/redis/go-redis/v9 v9.5.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
---
+++ apps/cron/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	goredis "github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+
+	redis := goredis.NewClient(&goredis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PWD"),
+		DB:       0,
+	})
+
+	// Publish
+	err := redis.Publish(ctx, "triger", "fire").Err()
+	if err != nil {
+		panic(err)
+	}
+
+	// Subscribe
+	pubsub := redis.Subscribe(ctx, "triger")
+	defer pubsub.Close()
+
+	sub := pubsub.Channel()
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := doGet()
+			if err != nil {
+				println("Error:", err.Error())
+			}
+		case msg := <-sub:
+			if msg.Payload == "fire" {
+				return
+			}
+		}
+	}
+}
+
+func doGet() error {
+	host := os.Getenv("TARGET_HOST")
+
+	client := resty.New()
+	resp, err := client.R().EnableTrace().Get("http://" + host)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 200 {
+		return err
+	}
+
+	return nil
+}
